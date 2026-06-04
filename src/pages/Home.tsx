fe0031f2587282a33868b5a1253c8f78bf6812fa
Guleesh Spithorst <i418162@fhict.nl>
2026-06-04 20:21:28 +0200

    commiting updated front-end pipeline with updated text in dev.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>FestiPlanner IS FINALLY UP AND RUNNING</h1>
+      <h1>FestiPlanner deployment is fantastic!</h1>
       <p>React is working correctly.</p>
     </div>
   )
