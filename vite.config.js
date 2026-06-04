076a99085f3598fdc046ac7ccb27e8a6080417bc
Guleesh Spithorst <i418162@fhict.nl>
2026-06-04 23:53:36 +0200

    updated vite config to recognize env

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.VITE_BASE_PATH || '/',
+  }
 })
